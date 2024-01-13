5a40abd45524201bb6f0a4d3b0edcdb8112b6376
bigproblems55 <pro@core>
2024-01-13 10:14:01 -0800

    Styled spinner components

--- fuckspinner/src/Core/Core.jsx
+++ fuckspinner/src/Core/Core.jsx
@@ -17,60 +17,69 @@ const Circle = styled.div`
       background-color: #00ff00;
       width: 200px;
       height: 200px;
-      border-radius: 1000px; 
+      border-radius: 400px; 
       margin: 0.5em;
 
-`
 
-const Aligner = styled.div`
+`
+const Bluedot = styled.div`
+
 /*Spinner Style */
   /* Blue dot*/
-  .aligner span{ 
-    display:inline;
+  left: 18%;
+  right:0%;
+  top:90%;
+  bottom:0%; 
+    
     position:absolute;
-    height:12px;
-    width:12px;
+    height:20%;
+    width:100%;
     background-color:blue;
-    border-radius:120px;
-  }
-  /*Spinner in the circle gets bigger yellowish*/
-  .aligner {
-    height: 80px;
-    width:10px;
-    background-color:yellow;
-    animation: circleit linear var(--ansec) ;
-  }
-  .container div.circle div.aligner{
-    position:absolute;
-    left:92px;
-    right:0px;
-    top:60px;
-    bottom:0px;
-    z-index:1;
-    transform:rotate(var(--rot3));
-  }
+    border-radius:100px;
   
-  /*Spinner Keyframe Animations*/
-  @keyframes circleit {
-    1% {
-    transform: rotate(var(--rot1));
-    } 
-    25% {
-      transform: rotate(var(--rot2));
-    }
-    100%{
-      transform: rotate(var(--rot3));
-    } 
-  }
 `
+
+const Aligner = styled.span`
+
+/*Spinner in the circle gets bigger yellowish*/
+
+  height: 40%;
+  width:6%;
+  background-color:yellow;
+  animation: circleit linear 4s ;
+
+
+  position:absolute;
+  left:50%;
+  right:0%;
+  top:30%;
+  bottom:0%;
+  z-index:1;
+  transform:rotate(160deg);
+
+
+/*Spinner Keyframe Animations*/
+@keyframes circleit {
+  1% {
+  transform: rotate(10deg);
+  } 
+  25% {
+    transform: rotate(230deg);
+  }
+  100%{
+    transform: rotate(450deg);
+  } 
+}
+`
+
 export default function Core(){
 
     return(
         <Container>
     <Circle>
-        <div className="aligner">
-            <span id="spinner"></span>
-        </div>
+        
+            <Aligner><Bluedot /></Aligner>
+        
     </Circle>
     </Container>
     );
