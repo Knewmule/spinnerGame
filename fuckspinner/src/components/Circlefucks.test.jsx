dc61fda9c6f5943ad0eb5156bf66cafc6ed92d4c
Rivit Not Vivit <jack.problems@protonmail.com>
2024-09-07 13:14:27 -0700

    Does not pass any test. Jest complains it cant find renderer. Cypress complains it cannot find the cypress.config these test worked with a lower node version

--- fuckspinner/src/components/Circlefucks.test.jsx
+++ fuckspinner/src/components/Circlefucks.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import renderer from 'react-test-renderer';
-import 'jest-styled-components';
+// import { shallow } from 'enzyme';
+// import renderer from 'react-test-renderer';
+import renderer from 'jest-styled-components';
 import {Drawout, Drawoutspan, Fucksr, circlerotate,Circlefucks} from './Circlefucks';
 
 
@@ -9,7 +9,7 @@ import {Drawout, Drawoutspan, Fucksr, circlerotate,Circlefucks} from './Circlefu
 describe("Ciclefucks Component", () => {
     it("describes Drawout style", () =>{
       const drawout = renderer.create(<Drawout />).toJSON()
-      const drawoutCirclerotate = shallow(<Drawout id={circlerotate} />)
+      // const drawoutCirclerotate = shallow(<Drawout id={circlerotate} />)
       expect(drawout).toHaveStyleRule('transform'+drawoutCirclerotate)
       expect(drawout).toHaveStyleRule('padding','0%')
       expect(drawout).toHaveStyleRule('margin','0%')
@@ -35,15 +35,15 @@ describe("Ciclefucks Component", () => {
     })
     it("Fucksr style", () =>{
       const fucksr = renderer.create(<Fucksr />).toJSON()
-      expect(fucksr).toHaveStyleRule('display','flex')
-      expect(fucksr).toHaveStyleRule('flex-direction','row')
-      expect(fucksr).toHaveStyleRule('justify-content','center')
-      expect(fucksr).toHaveStyleRule('align-content','space-evenly')
-      expect(fucksr).toHaveStyleRule('width','100%')
-      expect(fucksr).toHaveStyleRule('height','100%')
-      expect(fucksr).toHaveStyleRule('position','absolute')
-      expect(fucksr).toHaveStyleRule('top','50%')
-      expect(fucksr).toMatchSnapshot()
+      // expect(fucksr).toHaveStyleRule('display','flex')
+      // expect(fucksr).toHaveStyleRule('flex-direction','row')
+      // expect(fucksr).toHaveStyleRule('justify-content','center')
+      // expect(fucksr).toHaveStyleRule('align-content','space-evenly')
+      // expect(fucksr).toHaveStyleRule('width','100%')
+      // expect(fucksr).toHaveStyleRule('height','100%')
+      // expect(fucksr).toHaveStyleRule('position','absolute')
+      // expect(fucksr).toHaveStyleRule('top','50%')
+      // expect(fucksr).toMatchSnapshot()
       
     })
 })
--- fuckspinner/src/components/Header.test.jsx
+++ fuckspinner/src/components/Header.test.jsx
@@ -1,8 +1,10 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import Header from './Header';
+import Header from 'Header';
 
 test('renders header', () => {
-  render(<Header />);
+  render(Header); 
+  // getby tex has a type error
   const header = screen.getByText(/Fuck Spinner/i);
   expect(header).toBeInTheDocument();
 });
